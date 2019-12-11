ee92fd6db69c6536652fece3c49c412ae9cb1c06
Max丶 <56401610+Max6411@users.noreply.github.com>
2019-12-11 16:32:42 +0800

    Update BASIC-8	回文数.java

--- 基础练习/BASIC-8	回文数.java
+++ 基础练习/BASIC-8	回文数.java
@@ -4,6 +4,6 @@ public class Main
 	{
 		for(int i=1000 ; i < 10000 ; ++i)
 			if(i/1000 == i%10 && i/100%10 == i/10%10)
-				System.out.println(i);
+				System.out.println(i);//题目没有说明输出要求 默认为和上一题一样:一行一个数据
 	}
 }
