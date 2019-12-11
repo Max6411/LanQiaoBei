5e529f2bb55d0a3e010de073cfb8be186839308f
Max丶 <56401610+Max6411@users.noreply.github.com>
2019-12-11 15:41:43 +0800

    Update BASIC-3	字母图形.java

--- 基础练习/BASIC-3	字母图形.java
+++ 基础练习/BASIC-3	字母图形.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-//注意图形列数大于行数的情况
-
 public class Main
 {
 	public static void main(String[] args)
@@ -9,26 +6,14 @@ public class Main
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int m = in.nextInt();
+		char ch = 'A';
 		for(int i=0 ; i<n ; ++i)
 		{
-			PrintFront(i,m);//第i行前部分打印 (i<=m) ? i : m 个
-			PrintRear(m-i);
+			for(int j=0 ; j<m ; ++j)
+				System.out.print((char)(Math.abs(i-j)+'A'));
+			System.out.println();
 		}
 		in.close();
 	}
 
-	private static void PrintRear(int n)
-	{
-		char ch = 'A';
-		for(int i=0 ; i<n ; ++i)
-			System.out.print((char)(ch+i));
-		System.out.println();
-	}
-
-	private static void PrintFront(int n,int m)
-	{
-		char ch = 'A';
-			for(int i=n ; i>((n > m) ? n-m : 0); --i)
-				System.out.print((char)(ch+i));
-	}
 }
