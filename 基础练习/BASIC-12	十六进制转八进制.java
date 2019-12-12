62d747f361a0ca1f7252a1fd9a99f323aaa97e2f
Max丶 <56401610+Max6411@users.noreply.github.com>
2019-12-12 14:20:47 +0800

    Create BASIC-12	十六进制转八进制.java

---
+++ 基础练习/BASIC-12	十六进制转八进制.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int []arr = new int[n];
+		for(int i=0 ; i<n ; ++i)
+			arr[i] = in.nextInt();
+		Arrays.sort(arr);
+		for(int i=0 ; i<n ; ++i)
+		{
+			if(i != 0) System.out.print(" ");
+			System.out.print(arr[i]);
+		}
+		in.close();
+	}
+}
